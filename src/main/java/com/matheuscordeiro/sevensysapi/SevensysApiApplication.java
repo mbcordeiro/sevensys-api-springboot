6297a9bdd437556d8892e9e0d1d85daae784b5b1
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2021-07-09 18:28:46 -0300

    First commit

---
+++ src/main/java/com/matheuscordeiro/sevensysapi/SevensysApiApplication.java
@@ -0,0 +1,13 @@
+package com.matheuscordeiro.sevensysapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SevensysApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SevensysApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matheuscordeiro/sevensysapi/SevensysApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheuscordeiro.sevensysapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SevensysApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
